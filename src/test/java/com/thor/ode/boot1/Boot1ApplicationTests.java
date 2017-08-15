c6598fc867a497308bc8cf0ee919606ce149ac20
zhls <1163846211@qq.com>
2017-08-15 19:38:53 +0800

    dev tools

--- src/test/java/com/thor/ode/boot1/Boot1ApplicationTests.java
+++ src/test/java/com/thor/ode/boot1/Boot1ApplicationTests.java
@@ -1,16 +1,36 @@
 package com.thor.ode.boot1;
 
+import com.thor.ode.boot1.controller.TestController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Boot1ApplicationTests {
 
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception{
+		mvc = MockMvcBuilders.standaloneSetup(new TestController()).build();
+	}
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getHello() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+	}
 }
