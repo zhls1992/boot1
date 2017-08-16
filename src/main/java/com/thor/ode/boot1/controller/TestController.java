91ad1bab39f119a2e0ade003ed149c38f1aa4ae3
zhls <1163846211@qq.com>
2017-08-16 17:10:35 +0800

    redis session 共享

--- src/main/java/com/thor/ode/boot1/controller/TestController.java
+++ src/main/java/com/thor/ode/boot1/controller/TestController.java
@@ -1,10 +1,19 @@
 package com.thor.ode.boot1.controller;
 
+import com.thor.ode.boot1.dao.UserRepository;
+import com.thor.ode.boot1.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
 @RestController
 public class TestController {
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/hello")
     public String hello(){
@@ -16,4 +25,22 @@ public class TestController {
         return "so easy!";
     }
 
+    @RequestMapping("/getUser")
+    @Cacheable(value="user-key")
+    public User getUser() {
+
+        User user=userRepository.findByUserName("aa");
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
+        return user;
+    }
+
+    @RequestMapping("/uid")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return session.getId();
+    }
 }
